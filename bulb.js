45153ea81b32ae477a290cde20a922d76174d244
anupam <anupammahato357@gmail.com>
2023-09-22 23:49:46 +0530

    first commit

---
+++ bulb.js
@@ -0,0 +1,12 @@
+
+function bulbOn(){
+    let light=document.querySelector(".bulb-off");
+    console.log(light)
+    light.setAttribute("src","img/bulb1-on.jpg");
+}
+function bulbOff(){
+    let light=document.querySelector(".bulb-off");
+    console.log(light)
+    light.setAttribute("src","img/bulb1-off-removebg.png");
+}
+
